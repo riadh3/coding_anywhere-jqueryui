82d44ee56991201fafa299405213f0a4df85d524
riadh3 <riadh.dvl@gmail.com>
2019-05-26 18:47:07 +0100

    third commit

--- js/script.js
+++ js/script.js
@@ -1,19 +1,59 @@
-$(document).ready(()=>{
+$(document).ready(() => {
 
-    $('#toggle-btn').on('click', function(){
+    $('#toggle-btn').on('click', function () {
         $('.nav-links').stop().slideToggle(2000)
     })
 
-// accordion
-$('#accordion').accordion()
+    // accordion
+    $('#accordion').accordion()
 
-// magnific popup
-$('#current-center').magnificPopup({
-    delegate: 'a', /* from https: *///dimsemenov.com/plugins/magnific-popup/documentation.html
-    type: 'image',
-    gallery: {
-        enabled: true
-      }
-  })
+    // magnific popup
+    $('#current-center').magnificPopup({
+        delegate: 'a',
+        /* from https: */ //dimsemenov.com/plugins/magnific-popup/documentation.html
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    })
+
+    // progress bars for the prgress HTMLTableSectionElement,
+    // plugin from jqueryui
+    $('#prog-bar-1').progressbar({
+        value: 20
+    })
+    $('#prog-bar-2').progressbar({
+        value: 40
+    })
+    $('#prog-bar-3').progressbar({
+        value:60 
+    }) 
+    $('#prog-bar-4').progressbar({
+        value: 80
+    })
+
+    // owlCarousel for the quote section
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        smartSpeed:2000,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+
+
+
+
+
 
 })
